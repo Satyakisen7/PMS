bdd5fd493b33d53c80610de61ce892538fe27322
sensa <sensa@SATYAKI>
2022-03-26 00:27:35 +0530

    Add project files.

---
+++ Company.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PMS
+{
+    abstract class Company
+    {
+        public int addDepartment(string deptName) { return addDepartment(deptName); }
+        public int addEmployee(IEmployee e, out int id) { return addEmployee(e, out id); }     
+        public int editEmployee(int employee) { return editEmployee(employee); }
+
+        public interface IEmployee
+        {
+            viewEmployee(int id) 
+
+            viewEmployee(string name) 
+        }
+        public int deleteEmployee(int id) { return deleteEmployee(id); }
+        public int viewStatistics(int id) { return viewStatistics(id); }
+    }
+}
---
+++ Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PMS
+{
+    public class Employee
+    {
+        public int EmployeeID { get; set; }
+        public string FullName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public int Experience { get; set; }
+        public string Designation {get; set; }
+        public int BasicPay { get; set; }
+        public int GrossPay { get; set; }
+        public int DepartmentID { get; set; }
+
+    }
+}
